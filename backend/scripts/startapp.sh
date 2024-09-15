505c31e77e48592b2782c13c06364b53a1c48ca0
arseniy <a.moguchii@mail.ru>
2024-09-15 07:55:07 +0300

    Create startapp.sh

---
+++ backend/scripts/startapp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+APP_NAME=$1
+
+mkdir -p cmd/$APP_NAME
+touch cmd/$APP_NAME/main.go
+
+mkdir -p internal/$APP_NAME/mdl
+touch internal/$APP_NAME/mdl/mdl.go
+
+mkdir -p internal/$APP_NAME/repo
+touch internal/$APP_NAME/repo/repo.go
+
+mkdir -p internal/$APP_NAME/svc
+touch internal/$APP_NAME/svc/svc.go
+
+mkdir -p internal/$APP_NAME/urls
+touch internal/$APP_NAME/urls/urls.go
+
+mkdir -p internal/$APP_NAME/ctl
+touch internal/$APP_NAME/ctl/ctl.go
