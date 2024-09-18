a2859cd6dd8d1803a2c4f5d9f4f7ed6a9e35889b
arseniy <a.moguchii@mail.ru>
2024-09-18 12:21:08 +0300

    optional parse .env

--- backend/internal/cfg/cfg.go
+++ backend/internal/cfg/cfg.go
@@ -3,28 +3,33 @@
 package cfg
 
 import (
-    "path/filepath"
-    "runtime"
-    "log"
-    "os"
-    "github.com/joho/godotenv"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/joho/godotenv"
 )
 
 var (
-    RootDir     string
-    DatabaseUrl string
+	RootDir     string
+	DatabaseUrl string
 )
 
 func init() {
 	// init RootDir
-    _, b, _, _ := runtime.Caller(0)
-    RootDir = filepath.Join(filepath.Dir(filepath.Dir(b)), "..")
+	_, b, _, _ := runtime.Caller(0)
+	RootDir = filepath.Join(filepath.Dir(filepath.Dir(b)), "..")
 
-	// parse .env
-    envPath := filepath.Join(RootDir, ".env")
-    if err := godotenv.Load(envPath); err != nil {
-        log.Fatal("Error loading .env file:", err)
-    }
+	// parse .env if exists locally
+	envPath := filepath.Join(RootDir, ".env")
+	if _, err := os.Stat(envPath); err == nil {
+		if err := godotenv.Load(envPath); err != nil {
+			log.Fatal("Error loading .env file:", err)
+		}
+	} else {
+		log.Println(".env file not found, using system environment variables.")
+	}
 
-    DatabaseUrl = os.Getenv("DATABASE_URL")
+	DatabaseUrl = os.Getenv("DATABASE_URL")
 }
